a2452607b50edc88056948b535dd427f606f02b4
Bruno De Masi <brunodemasi1@gmail.com>
2020-04-10 01:38:23 -0300

    docs: docs: change CPF export to export default

--- lib/br/CPF/index.ts
+++ lib/br/CPF/index.ts
@@ -2,4 +2,4 @@ import validate from './CPFValidator';
 import format from './CPFFormatter';
 import generate from './CPFGenerator';
 
-export { validate, format, generate };
+export default { validate, format, generate };
